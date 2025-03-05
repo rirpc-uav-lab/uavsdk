191b723ddea3d4f1e4d6ff26cbc5aa9ab2648a90
Leshchev <yyootttaa@gmail.com>
2025-03-05 14:26:24 +0300

    1

--- include/uavsdk/data_adapters/cxx/cxx.hpp
+++ include/uavsdk/data_adapters/cxx/cxx.hpp
@@ -40,6 +40,7 @@ namespace uavsdk
             template <typename T>
             class PointerAccessDataAdapter : BasicDataAdapter<T>
             {
+                public:
                 PointerAccessDataAdapter(T initial_data) : BasicDataAdapter<T>(initial_data)
                 {
                     this->___set_type();
