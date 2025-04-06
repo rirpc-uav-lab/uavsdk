70bc96701c05891b15a79d0a6451576771dab19a
Andrey <yyootttaa@gmail.com>
2025-04-06 12:41:34 +0300

    Refactored useful_data_interfaces.hpp

---
+++ include/uavsdk/useful_data_lib/base_interfaces.hpp
@@ -0,0 +1,113 @@
+
+#pragma once
+#include <memory>
+// #include <map>
+
+#include <uavsdk/utils/cpp_custom_extras.hpp>
+
+
+
+namespace useful_di
+{
+    /**
+     * @brief Интерфейс типа. Любой класс, являющися объектом данных и 
+     * предполагающий совместимость с композитной системой библиотеки useful_di, 
+     * должен напрямую или не напрямую унаследовать от этого класса. Каждый 
+     * класс наследник должен определить функцию ___set_type() и вызывать ее, 
+     * например, в конструкторе, чтобы можно было идентифицировать тип объекта 
+     * данных
+     */
+    class TypeInterface
+    {
+        public:
+        TypeInterface()
+        {
+            // this->___set_type();
+        }
+
+
+        /**
+         * @brief Возвращает строку, хранящую тип данного объекта, заданный
+         * с помощью функции ___set_type()
+         */
+        std::string ___get_type() 
+        {
+            if (not this->___type.empty()) return this->___type;
+            else throw std::runtime_error("TypeInterface: _set_type was not called before get_type");
+        }
+
+
+        protected:
+        std::string ___type; // Тип этого объекта данных
+
+
+        /**
+         * @brief Этот метод указывает тип текущего объекта. 
+         * Его необходимо определять в "листьях" - финальных реализациях 
+         * последовательности интерфейсов, образованной наследованием 
+         * от этого класса. Если в своей реализации этого метода вы
+         * не уверены, финальная ли она, сделайте реализацию этого метода
+         * виртуальной, чтобы ее можно было переопределить
+         */
+        virtual void ___set_type()
+        {
+            this->___type = typeid(*this).name();
+        };
+    };
+
+
+    template <typename Retrieved>
+    class IRetrievableDataContainer
+    {
+    public:
+        virtual Retrieved get_data() = 0;
+    };
+
+
+
+    template <typename Id>
+    class IAppendAbleContainer
+    {
+        public:
+        virtual Id add_data(const std::shared_ptr<TypeInterface>& _data) = 0;
+    };
+
+
+    template <typename Id>
+    class IInsertAbleContainer
+    {
+        public:
+        virtual void add_data(const Id _key, const std::shared_ptr<TypeInterface>& _data) = 0;
+    };
+
+
+    template <typename Id>
+    class IRemoveAbleContainer
+    {
+        public:
+            virtual void remove_data(const Id& data_identifier) = 0;
+    };
+
+
+    template <typename Id>
+    class IAtAbleContainer
+    {
+        public:
+            virtual std::shared_ptr<TypeInterface> at(const Id& data_identifier) = 0;
+    };
+
+
+    class ISizeAbleContainer
+    {
+        public:
+            virtual size_t size() = 0;
+    };
+
+
+    template <typename Id>
+    class IModifyAbleContainer
+    {
+        public:
+            virtual void modify_data(const Id& data_identifier, const std::shared_ptr<TypeInterface>& _data) = 0;
+    };
+} // namespace useful_di
